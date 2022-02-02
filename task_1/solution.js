e33dbbfba0dbbcc6eb88b27b2d8cb8eed53b2bcf
partozan <fomin19771@gmail.com>
2022-02-02 14:06:59 +0500

    888

--- task_1/solution.js
+++ task_1/solution.js
@@ -1,24 +1,27 @@
 function addInCartMessage(title, price) {
     let productName = title;
     let productPrice = price;
-    let message 
-    message = productName + ' ' + 'за'+ ' ' + productPrice + ' ' + 'теперь в корзине!'
-console.log(message)
-
-
+     
     // Задание №1.1. Оповещение о добавлении в корзину
 
     // создайте переменную message
+    let message
     // присвойте переменной message значение переменной productName
+    message = productName
     // добавьте в конец переменной message пробел
+    message = message + ' ';
     // добавьте в конец переменной message текст 'за'
+    message = message + 'за';
     // добавьте в конец переменной message пробел
+    message = message + ' ';
     // добавьте в конец переменной message значение переменной productPrice
+    message = message + productPrice;
     // добавьте в конец переменной message пробел
-    // hello word
+    message = message + ' ';
     // добавьте в конец переменной message текст 'теперь в корзине!'
+    message = message + 'теперь в корзине!';
     // выведите переменную message в консоль
-
+    console.log(message);
     // Конец решения задания №1.1.
 
     return message;
@@ -30,11 +33,11 @@ function addInCartCountChange(value) {
     let oldValue = value;
 
     // создайте переменную newValue
-    let newValue 
+    let newValue; 
     // присвойте ей значение oldValue
-    newValue = oldValue
+    newValue = oldValue;
     // увеличьте значение newValue на 1
-    newValue = newValue++
+    newValue = newValue + 1;
 
     // Конец решения задания №1.2.
 
@@ -48,12 +51,13 @@ function addInCartSumChange(sum, delta) {
     // Задание №1.3. Увеличение суммы в корзине
 
     // создайте переменную newSum и присвойте ей сумму oldSum и difference
-    let newSum 
+    let newSum
     newSum = oldSum + difference
     // создайте переменную newSumText и присвойте ей результат склеивания переменной newSum и текста ' ₽'
-    newSum = newSum + ' ₽'
+    let newSumText 
+    newSumText = newSum + ' ₽';
     // замените склеивание, используемое в newSum Text на шаблонную строку
-    //а это КАК? 
+    newSumText = `Сумма покупки составляет ${newSum} ₽`;
 
     // Конец решения задания №1.3.
 
